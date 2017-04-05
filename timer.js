71cdfb92e55d241a82474ada920427751d4d04ac
Quentin <quentin01960@orange.fr>
2017-04-05 09:05:52 +0200

    git c'est nul

--- timer.js
+++ timer.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-  var seconde=0;
-=======
-window.onload(){
+var seconde=0;
   var seconde=55;
->>>>>>> 79c9eaa2b9587cfc28a55c3de3f6e21fd318d185
   var minute=0;
   function Timer() {
     setInterval(flashText, 1000);
@@ -17,4 +13,3 @@ window.onload(){
     var elem = document.getElementById("timer").innerHTML="<p class=timer>"+minute+" min "+seconde+" s</p>";
     seconde +=1;
   }
-}
