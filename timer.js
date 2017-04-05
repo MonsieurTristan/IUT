f1da0186ee3ae2646dc784fb5b66b5f9c9da48d1
Quentin <quentin01960@orange.fr>
2017-04-05 09:13:17 +0200

    je debug encore git

--- timer.js
+++ timer.js
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
 var seconde=0;
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-  var seconde=0;
-=======
-window.onload(){
->>>>>>> ee2d0e6541b2976e07c03646c05c3c4316491981
-  var seconde=55;
   var minute=0;
   function Timer() {
     setInterval(flashText, 1000);
   }
->>>>>>> b847f44c4a256f49e7c315737d5ac8c8aa3ff170
 
-var seconde=0;
-var minute=0;
-
-<<<<<<< HEAD
   function flashText() {
     if(seconde==60){
       minute++;
@@ -27,20 +12,5 @@ var minute=0;
     var elem = document.getElementById("timer").innerHTML="<p class=timer>"+minute+" min "+seconde+" s</p>";
     seconde +=1;
   }
-=======
 
-function Timer() {
-  setInterval(flashText, 1000);
-}
-
-function flashText() {
-  if(seconde==60){
-    minute++;
-    seconde =0;
-  }
-  var elem = document.getElementById("timer").innerHTML="<p class=timer>"+minute+" min "+seconde+" s</p>";
-  seconde +=1;
-}
-
-Timer();
->>>>>>> ee2d0e6541b2976e07c03646c05c3c4316491981
+  Timer();
