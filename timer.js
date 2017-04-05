26645349e9d0e9975e2cbccac7a9b5c6d997f51a
Tristan KOPEC <Tristan KOPEC>
2017-04-05 09:03:21 +0200

    Such Checkers

--- timer.js
+++ timer.js
@@ -1,16 +1,19 @@
-window.onload(){
-  var seconde=55;
-  var minute=0;
-  function Timer() {
-    setInterval(flashText, 1000);
-  }
 
-  function flashText() {
-    if(seconde==60){
-      minute++;
-      seconde =0;
-    }
-    var elem = document.getElementById("timer").innerHTML="<p class=timer>"+minute+" min "+seconde+" s</p>";
-    seconde +=1;
-  }
+var seconde=0;
+var minute=0;
+
+
+function Timer() {
+  setInterval(flashText, 1000);
 }
+
+function flashText() {
+  if(seconde==60){
+    minute++;
+    seconde =0;
+  }
+  var elem = document.getElementById("timer").innerHTML="<p class=timer>"+minute+" min "+seconde+" s</p>";
+  seconde +=1;
+}
+
+Timer();
